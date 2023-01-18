ac29bd3ba98d2dcc8ac73e12ac517b62093deeb8
Nick Lauritzen <sirnicklaus@gmail.com>
2023-01-18 16:55:41 -0700

    Added social icons, styled footer, and scripted copyright year text.

---
+++ index.js
@@ -0,0 +1,4 @@
+// var currentYear = new Date().getFullYear();
+// document.getElementById("copyrightText").innerHTML = "&#9400 Cutting Edge Ammo " + currentYear;
+// // Put in a script tag in index.html itself.
+
