312372e277fdb265f90c9e488e97d030244611d0
Nick Lauritzen <sirnicklaus@gmail.com>
2023-01-20 14:31:13 -0700

    Added some code to index.js to alter the numOfItems inner HTML in the header.

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 // document.getElementById("copyrightText").innerHTML = "&#9400 Cutting Edge Ammo " + currentYear;
 // // Put in a script tag in index.html itself.
 
+var numOfItems = 2;
+document.querySelector("#numOfItems").innerHTML = "&#128722 " + numOfItems + " ITEMS";
