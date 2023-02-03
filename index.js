0a474b0a43f0a4ad9bce4222220b910158add6af
Nick Lauritzen <sirnicklaus@gmail.com>
2023-02-03 15:47:13 -0700

    Wrote own slideShow function to make slideshow automatic.  Need to make also manual.

--- index.js
+++ index.js
@@ -37,47 +37,55 @@ document.querySelector("#numOfItems").innerHTML = "&#128722 " + numOfItems + " I
 //     slides[slideIndex - 1].style.display = "block";
 // };
 
+
+
+
+
 var slides = document.querySelectorAll(".mySlides");
 var prev = document.querySelector(".prev");
 var next = document.querySelector(".next");
-prev.addEventListener('click', () => slideIndex -= 1);
-next.addEventListener('click', () => slideIndex += 1);
 
 var slideIndex = 0;
 console.log(slides);
 
 function slideShow() {
-    for (slideIndex = 0; slideIndex < slides.length; slideIndex++) {
-        console.log(slideIndex);
-        if (slideIndex < 0) {slideIndex = slides.length - 1};
-        if (slideIndex > slides.length - 1) {slideIndex = 0};
-        if (slideIndex === 0) {
-            slides[0].style.display = "block";
-            slides[1].style.display = "none";
-            slides[2].style.display = "none";
-            slides[3].style.display = "none";
-            setTimeout(slideShow, 2000);
-            // sleep(2000).then(() => {slideShow()});
-            // setTimeout(() => { slideIndex += 1; }, 5000);
-        };
-        if (slideIndex === 1) {
-            slides[0].style.display = "none";
-            slides[1].style.display = "block";
-            slides[2].style.display = "none";
-            slides[3].style.display = "none";
-        };
-        if (slideIndex === 2) {
-            slides[0].style.display = "none";
-            slides[1].style.display = "none";
-            slides[2].style.display = "block";
-            slides[3].style.display = "none";
-        };
-        if (slideIndex === 3) {
-            slides[0].style.display = "none";
-            slides[1].style.display = "none";
-            slides[2].style.display = "none";
-            slides[3].style.display = "block";
-        };
+    prev.addEventListener('click', () => slideIndex -= 1);
+    next.addEventListener('click', () => slideIndex += 1);
+    if (slideIndex < 0) {slideIndex = slides.length - 1};
+    if (slideIndex > slides.length - 1) {slideIndex = 0};
+    if (slideIndex === 0) {
+        slides[0].style.display = "block";
+        slides[1].style.display = "none";
+        slides[2].style.display = "none";
+        slides[3].style.display = "none";
+        setTimeout(slideShow, 2000);
+        slideIndex++
+        // sleep(2000).then(() => {slideShow()});
+        // setTimeout(() => { slideIndex += 1; }, 5000);
+    }
+    else if (slideIndex === 1) {
+        slides[0].style.display = "none";
+        slides[1].style.display = "block";
+        slides[2].style.display = "none";
+        slides[3].style.display = "none";
+        setTimeout(slideShow, 2000);
+        slideIndex++
+    }
+    else if (slideIndex === 2) {
+        slides[0].style.display = "none";
+        slides[1].style.display = "none";
+        slides[2].style.display = "block";
+        slides[3].style.display = "none";
+        setTimeout(slideShow, 2000);
+        slideIndex++
+    }
+    else if (slideIndex === 3) {
+        slides[0].style.display = "none";
+        slides[1].style.display = "none";
+        slides[2].style.display = "none";
+        slides[3].style.display = "block";
+        setTimeout(slideShow, 2000);
+        slideIndex++
     };
 };
 
